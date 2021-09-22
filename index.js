5cefd9826493d774e2958df39c2b2372912d5ba0
drdarian <dariantaban@gmail.com>
2021-09-21 22:07:37 -0700

    api testing

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hi there!');
+const fetchData = async () => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: 'd9a436d8',
+      s: 'avengers'
+    }
+  });
+
+  console.log(response.data);
+}
+
+fetchData();
