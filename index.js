5a941936d353c3181ee6967d4ddb8df95016a268
drdarian <dariantaban@gmail.com>
2021-09-22 11:29:49 -0700

    image and title rendering on api call

--- index.js
+++ index.js
@@ -11,9 +11,19 @@ const fetchData = async (searchTerm) => {
 
 const input = document.querySelector('input');
 
-const onInput = event => {
-  const movies = fetchData(event.target.value);
-  console.log(movies);
+const onInput = async event => {
+  const movies = await fetchData(event.target.value);
+
+  for (let movie of movies) {
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+      <img src="${movie.Poster}" />
+      <h1>${movie.Title}</h1>
+    `;
+
+    document.querySelector('#target').appendChild(div);
+  }
 };
 
 input.addEventListener('input', debounce(onInput, 500));
